f1819bea009325503484e24d42989ae041180b20
Kishore Hui <kishorehui0@gmail.com>
2025-09-15 15:59:56 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,32 @@
+// PalindromeCheck.java
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        sc.close();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+    }
+
+    // Function to check palindrome
+    public static boolean isPalindrome(int number) {
+        int original = number;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        return original == reversed;
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# clg-java
-clg class
